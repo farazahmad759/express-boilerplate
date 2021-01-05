37ce5ca20fa9ae301fdb34ab68f117a1fd45eb7b
Faraz Ahmad <farazahmad759@gmail.com>
2021-01-05 17:11:35 +0500

    Setup node-convict for config management

---
+++ config/config.js
@@ -0,0 +1,17 @@
+var convict = require("convict");
+
+convict.addFormat(require("convict-format-with-validator").ipaddress);
+
+// Define a schema
+var config = convict({
+  app: require("./app"),
+  auth: require("./auth"),
+  cors: require("./cors"),
+  database: require("./database"),
+  view: require("./view"),
+});
+
+// Perform validation
+config.validate({ allowed: "strict" });
+
+module.exports = config;
